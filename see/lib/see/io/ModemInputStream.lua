ffba3e4512d551d7ddabfccc9a1869d31303e351
sci4me <minecraftman7778@gmail.com>
2014-03-16 06:35:53 -0500

    better? hmm.. i think that may work better.. hmm

--- see/lib/see/io/ModemInputStream.lua
+++ see/lib/see/io/ModemInputStream.lua
@@ -14,7 +14,7 @@ function ModemInputStream:init(modem, channel)
 	self.running = true
 	self.thread = Thread:new(function()
 		while self.running do
-			local id = os.startTimer(1/80)
+			local id = os.startTimer(1/20)
 			local evt 
 			try(function()
 				evt = Events.pull("modem_message", "timer")
