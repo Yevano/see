0063032b21e4bc1172fa5812ea14e8138b23f0e9
sci4me <minecraftman7778@gmail.com>
2014-03-16 05:17:26 -0500

    --@import ftw

--- see/lib/see/io/peripheral/AbstractPeripheral.lua
+++ see/lib/see/io/peripheral/AbstractPeripheral.lua
@@ -1,3 +1,5 @@
+--@import see.io.Peripheral
+
 function AbstractPeripheral:init(location)
 	if not Peripheral.isPresent(location) then
 		throw(InvalidArgumentException:new(1, "see.base.String: location", "invalid location"))
