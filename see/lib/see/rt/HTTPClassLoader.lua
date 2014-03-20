c1a50727d90432ed277b92b9dd94a1493946d062
sci4me <minecraftman7778@gmail.com>
2014-03-19 21:14:48 -0500

    dont know why i did all that

--- see/lib/see/rt/HTTPClassLoader.lua
+++ see/lib/see/rt/HTTPClassLoader.lua
@@ -1,25 +1,22 @@
 --@import see.rt.ClassLoader
 --@import see.rt.DefaultClassLoader
---@import see.rt.FileClassLoader
 --@import see.net.HTTP
 --@import see.net.URL
---@import see.util.FastArray
---@import see.io.IOException
 
 --@native loadstring
 
---@extends see.rt.FileClassLoader
+--@extends see.rt.DefaultClassLoader
 
 function HTTPClassLoader:loadClass(url, name)
 	local response = HTTP.sync(url)
 	if response.responseCode ~= 200 then
-		throw(IOException:new("HTTP Response: " .. response.responseCode))
+		return nil, "HTTP Response: " .. response.responseCode
 	end
 
 	local code = response.body:lstr()
 	local def, err = loadstring(code, name)
 	if not def then
-		throw(IOException:new("Could not load class!"))
+		return nil, "Could not load class!"
 	end
 
 	return self:super(DefaultClassLoader).loadClass(def, ClassLoader.getAnnotations(code), name)
