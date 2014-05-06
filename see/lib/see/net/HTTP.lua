8d1cc24b4891cd082f53dd3502ebffb23a0cb49d
SquidDev <bonzoweb@hotmail.co.uk>
2014-05-06 20:50:06 +0100

    Add support for HTTP headers

--- see/lib/see/net/HTTP.lua
+++ see/lib/see/net/HTTP.lua
@@ -11,11 +11,12 @@
     Makes a blocking HTTP request.
     @param see.net.Url:url The URL to request from.
     @param see.base.String:postData Post data to send if this is a POST request.
+    @param see.base.Array Custom HTTP headers to send
     @return see.base.String The body of the response.
     @throw see.util.InvalidArgumentException if the args are incorrect.
     @throw see.io.IOException If the request fails.
 ]]
-function HTTP.sync(url, postData)
+function HTTP.sync(url, postData, headers)
     --[[if postData then postData = cast(postData, "string") end
     local f = postData and http.post or http.get
     local handle = f(url.string:lstr(), postData)
@@ -23,8 +24,9 @@ function HTTP.sync(url, postData)
 
     ArgumentUtils.check(1, url, URL)
     if postData then postData = cast(postData, "string") end
+    if headers then ArgumentUtils.check(3, headers, "table") end
     
-    HTTP.async(url, postData)
+    HTTP.async(url, postData, headers)
 
     local event = Events.pull(HTTPSuccessEvent, HTTPFailureEvent)
     if event.ident == "http_success" then
@@ -38,10 +40,12 @@ end
     Makes an asynchronous HTTP request.
     @param see.net.Url:url The URL to request from.
     @param see.base.String:postData Post data to send if this is a POST request.
+    @param see.base.Array Custom HTTP headers to send
     @throw see.util.InvalidArgumentException if the args are incorrect.
 ]]
-function HTTP.async(url, postData)
+function HTTP.async(url, postData, headers)
     ArgumentUtils.check(1, url, URL)
     if postData then postData = cast(postData, "string") end
-    http.request(url.string:lstr(), postData)
+    if headers then ArgumentUtils.check(3, headers, "table") end
+    http.request(url.string:lstr(), postData, headers)
 end
