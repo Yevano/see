8bf5846022131b573eb110f824663c33e5913fee
SquidDev <bonzoweb@hotmail.co.uk>
2014-05-10 19:14:15 +0100

    Revert "Added old source files."
    This reverts commit ea2b7c20b92fbc564c8247b1483883e5859e47e7.

--- see/lib/see/linknet/net/MessageReceiver.lua
+++
@@ -1,7 +0,0 @@
-function MessageReceiver:init(discoverer)
-    self.discoverer = discoverer
-end
-
-function MessageReceiver:send(id)
-    
-end
--- see/lib/see/linknet/net/MessageSender.lua
+++
@@ -1,7 +0,0 @@
-function MessageSender:init(discoverer)
-    self.discoverer = discoverer
-end
-
-function MessageSender:send(id, message)
-    message = cast(message, String)
-end
