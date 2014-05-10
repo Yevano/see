2c38838c4a912c79ca22c17783e783e1fb60fdd9
sci4me <minecraftman7778@gmail.com>
2014-05-09 21:59:48 -0500

    security because security

--- see/lib/see/rt/security/SecurityManager.lua
+++ see/lib/see/rt/security/SecurityManager.lua
@@ -1,16 +1,21 @@
 --@import see.rt.security.PermissionPolicy
+--@import see.rt.security.SecurityException
 
 local managers = Array:new()
 
 --[[
     Checks whether the given permission is given by the active security managers.
     @param number:permission
+    @param any:varargs the arguments to check on the policy
 ]]
-function SecurityManager.check(permission)
+function SecurityManager.check(permission, ...)
     for manager in managers:iAll() do
         if manager.enabled then
-            if manager.permissions[permission] then
-                return false
+            local policies = manager.policies[permission]
+            for policy in policies:iAll() do
+                if not policy:check(...) then
+                    throw(SecurityException:new("")) --TODO exception message
+                end
             end
         end
     end
@@ -19,7 +24,7 @@ end
 
 function SecurityManager:init()
     self.enabled = false
-    self.permissions = { }
+    self.policies = { }
     managers:add(self)
 end
 
@@ -31,10 +36,15 @@ function SecurityManager:getEnabled()
     return self.enabled
 end
 
-function SecurityManager:addPolicy(permission, allowed)
-    self.permissions[permission] = not allowed
+function SecurityManager:addPolicy(permission, policy)
+    local policies = self.policies[permission]
+    if not policies then
+        policies = Array:new()
+        self.policies[permission] = policies
+    end
+    policies:add(policy)
 end
 
 function SecurityManager:getPolicies(permission)
-    return permissions[permission]
+    return policies[permission]
 end
